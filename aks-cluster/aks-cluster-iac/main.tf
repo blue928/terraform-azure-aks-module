561908396e5fab224100778fd0363bcf6794be73
Blue Presley <bluepresley@gastonia.com>
2022-03-04 16:50:27 -0500

    add backend and provider block, test for errors

--- aks-cluster/aks-cluster-iac/main.tf
+++ aks-cluster/aks-cluster-iac/main.tf
@@ -4,4 +4,14 @@ terraform {
         source = "hashicorp/azurerm"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "terraform-global-state-files"
+    storage_acount_name = "tfglobalstatefilessa"
+    container_name = "tfstatefilescontainername"
+    key = "aksclustermoduleci.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
 }
